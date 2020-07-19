f653b836249faf96f9ebb07d582d3bef3bfc6f67
Guagadu <guagadumc@ymail.com>
2020-07-19 17:27:53 -0400

    Tested calculating distance

--- test.py
+++ test.py
@@ -1,11 +1,16 @@
 import mysql.connector
 
+from math import sin, cos, sqrt, atan2, radians
+
+radius = 3443.9 #The radius of the earth in nautical miles
+
 db = mysql.connector.connect(database='routegen', user='routegen', password='easyPw123', host='127.0.0.1')
 cursor = db.cursor()
 
 code = ""
 
-rt = input("Enter departure and arrival: ")
+#rt = input("Enter departure and arrival: ")
+rt = "bikfekch"
 if len(rt) == 8:
     rt = [rt[:4], rt[4:]]
     code = "icao"
@@ -18,10 +23,21 @@ query = "select * from airport where a_{} = '{}' or a_{} = '{}';".format(code, r
 cursor.execute(query)
 rt = cursor.fetchall()
 
-query = "select * from route where r_src = '{}' and r_dest = '{}';".format(rt[0][0], rt[1][0])
-cursor.execute(query)
+src = [rt[0][5], rt[0][6]]
+dest = [rt[1][5], rt[1][6]]
 
-rt = cursor.fetchall()
 
-for i in rt:
-    query = "select l_icao, r_id, source.a_icao, dest.a_icao, source.r_plane from (route natural join airport) as source, (route natural join airport) as dest"
+lata = radians(src[0])
+longa = radians(src[1])
+latb = radians(dest[0])
+longb = radians(dest[1])
+
+dlon = longb - longa
+dlat = latb - lata
+
+a = sin(dlat/2)**2 + cos(lata) * cos(latb) * sin(dlon/2)**2
+c = 2  * atan2(sqrt(a), sqrt(1-a))
+
+distance = radius * c
+
+print("Src: {}, Dest: {}, Distance: {}".format(rt[0][0], rt[1][0], distance))

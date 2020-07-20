ab2aab5ec0c5f190518f829c9b677149ebeadf66
Guagadu <guagadumc@ymail.com>
2020-07-20 19:59:23 -0400

    Update update_database.ps1

--- update_database.ps1
+++ update_database.ps1
@@ -1,2 +1,5 @@
 Invoke-WebRequest https://raw.githubusercontent.com/jpatokal/openflights/master/data/airports.dat -o data/airports.dat
 Invoke-WebRequest https://raw.githubusercontent.com/jpatokal/openflights/master/data/airlines.dat -o data/airlines.dat
+Invoke-WebRequest https://raw.githubusercontent.com/jpatokal/openflights/master/data/airlines.dat -o data/countries.dat
+Invoke-WebRequest https://raw.githubusercontent.com/jpatokal/openflights/master/data/airlines.dat -o data/planes.dat
+Invoke-WebRequest https://raw.githubusercontent.com/jpatokal/openflights/master/data/airlines.dat -o data/routes.dat

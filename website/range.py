e98a1534d9d780e867055a57d5fffe31781f0a21
Guagadu <guagadumc@ymail.com>
2020-08-25 17:55:36 -0400

    First route finder completed

--- website/range.py
+++ website/range.py
@@ -26,16 +26,62 @@ def rangeResponse(data):
             }
         }
     
+    #mids = findMidpoints(data['dep'], data['arr'], legs)
     
-    return legs
+    #airports = []
+    #for i in mids:
+        #airports.append(findNrst(i[0], i[1], data['rwy']))
+    
+    #return mids
+    
+    return findNext([data['dep']], data['dep'], data['arr'], data['range'], data['rwy'])
 
+def findNext(route, stop, arr, maxRange, rwy):
+    stopCoords = coords(stop)
+    lat = stopCoords[0]
+    long = stopCoords[1]
+    calc = "(3443.9 * 2  * atan((sqrt(pow(sin((radians(a_lat) - radians({}))/2),2) + cos(radians({})) * cos(radians(a_lat)) * pow(sin((radians(a_long) - radians({}))/2),2)))/(sqrt(1-(pow(sin((radians(a_lat) - radians({}))/2),2) + cos(radians({})) * cos(radians(a_lat)) * pow(sin((radians(a_long) - radians({}))/2),2))))))".format(lat, lat, long, lat, lat, long)
+    query = "select *, round({}) as 'Distance' from airport where a_rwy is not null AND a_rwy > {} HAVING Distance < {} ORDER BY Distance DESC LIMIT 100;".format(calc, rwy, maxRange)
+    cursor.execute(query)
+    airports = list(cursor.fetchall())
+    best = [-1, stop, 999999]
+    for i in range(len(airports)):
+        dist = distance(airports[i][4], arr)
+        if  dist < best[2]:
+            best = [i, airports[i][4], dist]
+        print(best)
+    
+    route.append(best[1])
+    
+    if best[2] < int(maxRange):
+        route.append(arr)
+        return route
+    
+    return findNext(route, best[1], arr, maxRange, rwy)
+    
+    
+    
 def findNrst(lat, long, rwy):
     radius = 3443.9 #The radius of the earth in nautical miles
-    calc = "({} * 2  * atan((sqrt(pow(sin((a_lat - {})/2),2) + cos({}) * cos(a_lat) * pow(sin((a_long - {})/2),2)))/(sqrt(1-pow(sin((a_lat - {})/2),2) + cos({}) * cos(a_lat) * pow(sin((a_long - {})/2),2)))))".format(radius, lat, lat, long, lat, lat, long)
-    query = "select *, {} as 'Distance' from airport where a_rwy is not null ORDER BY Distance ASC LIMIT 50;".format(calc)
+    calc = "(3443.9 * 2  * atan((sqrt(pow(sin((radians(a_lat) - radians({}))/2),2) + cos(radians({})) * cos(radians(a_lat)) * pow(sin((radians(a_long) - radians({}))/2),2)))/(sqrt(1-(pow(sin((radians(a_lat) - radians({}))/2),2) + cos(radians({})) * cos(radians(a_lat)) * pow(sin((radians(a_long) - radians({}))/2),2))))))".format(lat, lat, long, lat, lat, long)
+    query = "select *, round({}) as 'Distance' from airport where a_rwy is not null AND a_rwy > {} ORDER BY Distance ASC LIMIT 50;".format(calc, rwy)
     cursor.execute(query)
-    for i in cursor.fetchall():
-        print(i)
+    out = cursor.fetchall()
+    for i in range(len(out)):
+        out[i] = list(out[i])
+    return out
+
+def findMidpoints(dep, arr, legs):
+    src = coords(dep)
+    dest = coords(arr)
+    
+    points = []
+    for i in range(1, legs):
+        print(i, legs)
+        points.append([(i/legs)*(src[0]+dest[0]), (i/legs)*(src[1]+dest[1])])
+    
+    return points
+
 
 def coords(airport):
     query = "select a_lat, a_long from airport where a_{} = '{}';".format(codeType(airport), airport)
@@ -72,7 +118,7 @@ def distance(dep, arr):
 
     distance = radius * c
 
-    print("Src: {}, Dest: {}, Distance: {}, midpoint: {}, {}".format(getAirportInfo(dep)[0], getAirportInfo(arr)[0], int(distance), degrees((lata+latb)/2), degrees((longa+longb)/2)))
+    #print("Src: {}, Dest: {}, Distance: {}, midpoint: {}, {}".format(getAirportInfo(dep)[0], getAirportInfo(arr)[0], int(distance), degrees((lata+latb)/2), degrees((longa+longb)/2)))
     return int(distance)
 
 
@@ -85,6 +131,6 @@ def codeType(code):
     else:
         return "error"
 
-data = b'{"dep":"bikf","arr":"jfk","range":"1250"}'
+data = b'{"dep":"klax","arr":"yssy","range":"2000", "rwy": "7000"}'
 print(rangeResponse(data))
-print(findNrst(52.3124008, -48.1922503, 3000))
+#print(findNrst(52.3124008, -48.1922503, 3000))

51aed49ae88380c1c5651bcca68fd5c39656c494
Niklas <niklas.byriel@gmail.com>
2020-08-28 16:53:58 -0400

    Update download.py

--- data/OnTimePerformance/download.py
+++ data/OnTimePerformance/download.py
@@ -1,8 +1,8 @@
+#Generate a Windows PowerShell script to download the full dataset from the Bureau of Transport Statistics
+
 with open('downlaods.ps1', 'w') as file:
     for i in range(1987, 2021):
         for j in range(1,13):
-            if i == 2020 and j > 5:
-                continue
             filename = 'On_Time_Reporting_Carrier_On_Time_Performance_1987_present_{}_{}.zip'.format(i,j)
             site = "https://transtats.bts.gov/PREZIP/{}".format(filename)
             file.write('Invoke-WebRequest {} -o {}\n'.format(site, filename))
